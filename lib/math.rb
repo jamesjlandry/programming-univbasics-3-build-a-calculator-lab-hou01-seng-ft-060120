952761b08b2cebfecc16da772b4f0548f91f1b42
James Landry <james.j.landry@outlook.com>
2020-05-18 16:46:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,22 +3,22 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-#{num1} - #{num2}
+"#{num1}".to_i - "#{num2}".to_i
 end
 
 def division(num1, num2)
-#num1 \ #num2
+"#num1".to_i \ "#num2".to_i
 end
 
 def multiplication(num1, num2)
-#{num1} * #{num2}
+"#{num1}".to_i * "#{num2}".to_i
 end
 
 def modulo(num1, num2)
-#{num1} % #{num2}
+"#{num1}".to_i % "#{num2}".to_i
 end
 
 def square_root(num)
- Math.sqrt#{num}
+ Math.sqrt"#{num}".to_i
 end
 
